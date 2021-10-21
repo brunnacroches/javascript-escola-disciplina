c7afd19c5a9e99ee1bec7a49a1fcac3562bdf412
brunnacroches <78552460+brunnacroches@users.noreply.github.com>
2021-10-21 18:39:22 -0300

    Add files via upload

---
+++ 9-Disciplinas.js
@@ -0,0 +1,13 @@
+var colecao_disciplinas = [     
+  { id: 1, nome: "Português",     carga_horaria: 240 },      
+  { id: 2, nome: "Matemática",    carga_horaria: 220 },      
+  { id: 3, nome: "História",      carga_horaria: 160 },     
+  { id: 4, nome: "Geografia",     carga_horaria: 140 },      
+  { id: 5, nome: "Química",       carga_horaria: 160 },      
+  { id: 6, nome: "Física",        carga_horaria: 150 },      
+  { id: 7, nome: "Inglês",        carga_horaria: 120 }  
+]; 
+
+console.log( colecao_disciplinas[1].id );  
+console.log( colecao_disciplinas[4].nome ); 
+console.log( colecao_disciplinas[6].carga_horaria );
